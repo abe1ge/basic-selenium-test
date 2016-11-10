a998589009cb330325e6894599003bd34d75d9bc
TimPerry1994 <timperry@fsmail.net>
2016-11-10 15:02:40 +0000

    First commit2

---
+++ Basic-Selenium-test/src/test/java/com/mysite/selenium/SeleniumHQTest.java
@@ -0,0 +1,31 @@
+package com.mysite.selenium;
+
+/**
+ * Created by Administrator on 10/11/2016.
+ */
+
+import io.ddavison.conductor.Browser;
+import io.ddavison.conductor.Config;
+import io.ddavison.conductor.Locomotive;
+import org.junit.Test;
+
+@Config(
+        browser = Browser.CHROME,
+        url     = "http://seleniumhq.org"
+)
+public class SeleniumHQTest extends Locomotive{
+    @Test
+    public void testDownloadLinkExists() {
+        validatePresent(HomePage.LOC_LNK_DOWNLOADSELENIUM);
+    }
+
+    @Test
+    public void testTabsExist() {
+        validatePresent(HomePage.LOC_LNK_PROJECTSTAB);
+        validatePresent(HomePage.LOC_LNK_DOWNLOADTAB);
+        validatePresent(HomePage.LOC_LNK_DOCUMENTATIONTAB);
+        validatePresent(HomePage.LOC_LNK_SUPPORTTAB);
+        validatePresent(HomePage.LOC_LNK_ABOUTTAB);
+
+    }
+}
